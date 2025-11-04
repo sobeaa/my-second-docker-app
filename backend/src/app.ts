d0eeb5c522f93eb5bf1b7a13a56e890633402982
Sondre Benjamin Aasen <699902+sobeaa@users.noreply.github.com>
2025-11-04 10:44:05 +0100

    feat: added task controllers; fix: fixed bug with JSON parsing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import {useExpressServer} from 'routing-controllers';
+import {TaskController} from "./controllers/task.controller.js";
+import {TasksController} from "./controllers/tasks.controller.js";
 import {errorHandler} from './middlewares/error.handler.js';
 import {HealthController} from './controllers/health.controller.js';
 import {PingController} from "./controllers/ping.controller.js";
@@ -12,12 +14,13 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
 app.use(morgan('dev'));
 
 useExpressServer(app, {
   routePrefix: '/api/v1',
-  controllers: [ // TODO: add more controllers
+  controllers: [
+    TaskController,
+    TasksController,
     HealthController,
     PingController,
   ],
