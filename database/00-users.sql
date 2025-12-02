7b7bb238653d3ebed741dc452f35a615b84a37a2
Dom <157792207+lunarjuun@users.noreply.github.com>
2025-12-02 13:10:52 +0100

    users table

---
+++ database/00-users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users 
+(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    last_login TIMESTAMPTZ,
+)
