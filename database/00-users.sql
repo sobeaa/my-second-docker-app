4f0b9c14a05eeb767494dc28ec00ccea59214f59
Dimitri Podgorny <dimapodgo@proton.me>
2025-12-02 09:51:58 +0100

    Databse: Updated 00-users.sql

---
+++ database/00-users.sql
@@ -0,0 +1,8 @@
+CREATA TABLE IF NOT EXISTS users
+(
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(50) NOT NULL
+);
