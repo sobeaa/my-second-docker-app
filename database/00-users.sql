c3a984a405b2efa5a7292b59e667bc5a2f21fa04
Sondre Benjamin Aasen <699902+sobeaa@users.noreply.github.com>
2025-12-09 09:34:53 +0100

    feat: changed max length of email and phone to 255

--- database/00-users.sql
+++ database/00-users.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users
     id         BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name  VARCHAR(100) NOT NULL,
-    email      VARCHAR(100) NOT NULL,
-    phone      VARCHAR(50)  NOT NULL
+    email      VARCHAR(255) NOT NULL,
+    phone      VARCHAR(255) NOT NULL
 );
